f019ac169de834205940e696e68821562a4aa35e
Juliandri04 <juliandrigifari@gmail.com>
2023-03-19 19:58:26 +0700

    Halo Generic

---
+++ TP_Modul5_1302210087/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Modul5_1302210087
+{
+    internal class HaloGeneric
+    {
+        public void SapaUser(string x)
+        {
+            Console.WriteLine("Halo User " + x);
+        }
+    }
+}
---
+++ TP_Modul5_1302210087/Program.cs
@@ -0,0 +1,8 @@
+﻿using TP_Modul5_1302210087;
+
+string nama = Console.ReadLine();
+HaloGeneric haloGeneric = new HaloGeneric();
+haloGeneric.SapaUser(nama);
+
+DataGeneric<string> dataGeneric = new DataGeneric<string>("1302210087");
+dataGeneric.PrintData();
